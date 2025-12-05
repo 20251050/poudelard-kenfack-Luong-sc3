ce3723adcc22f6210be094b0f0426cc19b132d71
kenfa <theresine.kenfack@efrei.net>
2025-12-05 10:20:54 +0100

    ihiuh

--- main.py
+++
@@ -1,7 +0,0 @@
-from personnage import *
-
-nom = demander_nom("Entrez le nom de votre personnage : ")
-
-courage = demander_courage("Niveau de courage (1-10) : ")
-
-choix = demander_choix("Voulez-vous continuer ?", ["Oui", "Non"])
